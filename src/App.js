84ac905f13e0d52a35b31fd726777cd422e06b7a
ZuzanaOchmanova <zuzkaochmanova1@gmail.com>
2021-04-22 12:40:18 +0200

    Adding css and adding URL for the applications

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to J.A.R.V.I.S. </h1> 
+      <br></br>
+      <h2> What do you wish me to do?</h2>
+       <br></br>
+       </header>
+       <div className="row">
+         <div className="col-12">
+      <button className="btn btn-primary" type="button">
+       <a  href="https://sharp-fermi-a23368.netlify.app/" target="_blank" rel="noreferrer" >SEE WEATHER </a>
+    </button>    
+    <button className="btn btn-primary" type="button">
+       <a  href="https://youthful-mcnulty-935aa1.netlify.app/" target="_blank" rel="noreferrer" >DICTIONARY</a>
+    </button>      
+    </div>
+    </div>
+      
+      </div>
     </div>
   );
 }
